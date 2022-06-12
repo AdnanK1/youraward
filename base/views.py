1ee7de2506166a093147cc613c26e42b298f98b4
AdnanK1 <adnangitonga@gmail.com>
2022-06-12 15:23:22 +0300

    Base/Templates/Auth: added a form in register page

--- base/views.py
+++ base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     context = {}
     return render(request,'home.html',context)
@@ -10,5 +17,6 @@ def loginPage(request):
     return render(request,'auth/login.html',context)
 
 def register(request):
-    context = {}
+    form = CreateUserForm()
+    context = {'form':form}
     return render(request,'auth/register.html',context)
